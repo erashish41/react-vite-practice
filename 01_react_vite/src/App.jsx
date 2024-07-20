0355cb587277c4bb9ba5ab9963e6b68d7fd5cfc6
Ashish Bhardwaj <erashish41@gmail.com>
2024-07-20 23:18:09 +0530

    react vite started

---
+++ 01_react_vite/src/App.jsx
@@ -0,0 +1,11 @@
+export const App = () => {
+  return (
+      <div>
+        <image src= "https://m.media-amazon.com/images/M/MV5BYzc0N2VmYjQtY2Y0Mi00NzBhLTk3YjctNDI4MTU2YWJkYWFmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"/>
+        <h2>Name: Love Alarm</h2>
+        <h3>Seasons: 2</h3>
+        <h3>Rating: 6.6</h3>
+        <p>Love Alarm is the first Korean drama series confirmed for pick-up by Netflix. The first season of the series was released on Netflix on August 22, 2019. It was ranked as one of Netflix's top releases and was renewed for a second season in October 2019; season 2 was released on March 12, 2021.  In 2021, the series managed to rank as the 6th most watched K-dramas worldwide in the platform. In 2022, it was the ninth most searched Korean drama worldwide.</p>
+      </div>
+  )
+}
