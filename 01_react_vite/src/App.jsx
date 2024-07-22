4415ae8bed436fd55e740d18824e0b425b7a788a
Ashish Bhardwaj <erashish41@gmail.com>
2024-07-22 12:50:37 +0530

    conditional rendering in JSX

--- 01_react_vite/src/App.jsx
+++ 01_react_vite/src/App.jsx
@@ -1,42 +1,45 @@
 //Dynamic value: here we made variables function inside JS with the help of { } curely braces in JSX
+
 export const App = () => {
- 
-  return (
-      <>``
-        <NetflixSeries />
+    return (
+      <>
         <NetflixSeries />
         <NetflixSeries />
         <NetflixSeries />
         <NetflixSeries />
       </>
-  )
+    )
 }
+
 
 // NetflixSeries is a Component not a Function
 const NetflixSeries = () => {
-
   const naam = "Love Alarm";
-  const season = "2";
-  const rating = "6.6";
-  const summary = "Love Alarm is the first Korean drama series confirmed for pick-up by Netflix";
-
+  const season = 2;
+  const language = "k - drama";
+  const rating = 6.6;
   const returnGenre = () => {
     const genre = "Romantic & Commedy";
     return genre;
   }
-  
+  const age = 28;
+  const personAge = () => {
+    if(age >= 18) return "Watch now";
+    return "Not Available"
+  }
   return (
-    <div> 
     <div>
-          <img src="Love_Alarm.jpg"/>
-        </div>
-        <div>
-          <h2>Naam: {naam} </h2>
-          <h3>Seasons: {season} </h3>
-          <h3>Rating: {3 + 3.2} </h3>
-          <p>Summary: {summary}</p>
-          <p>Genre: {returnGenre()} </p>
-        </div>
-        </div>
+      <div>
+        <img src="Love_Alarm.jpg"/>
+      </div>
+      <div>
+        <h2>Naam: {naam}</h2>
+        <h3>Seasons: {season} </h3>
+        <h3>Language: {language} </h3>
+        <h3>Rating: {3 + 3.2} </h3>
+        <h3>Genre: {returnGenre() }</h3>
+        <button>{personAge() }</button>
+      </div>
+    </div>
   )
 }
